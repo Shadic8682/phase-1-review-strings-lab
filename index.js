29370533ad74c21b6de0f8a314c0ee51750e441b
JaQuan Banks <shadic151@gmail.com>
2022-10-12 09:16:12 -0400

    commit tree 90587e045d5c5659dec101f52aff11de904bf901

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser = "JaQuan Banks"
+var welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+var excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+var shortGreeting = `Welcome, ${currentUser[0]}!`
